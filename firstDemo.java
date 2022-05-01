777f84034277b2a1cd989dba7b64ba37f5efd358
Shresthhooda <shooda@ucsd.edu>
2022-04-30 21:30:09 -0700

    failing test

--- firstDemo.java
+++ firstDemo.java
@@ -1,1 +1,8 @@
+public class firstDemo{
 
+
+    public static int subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ firstDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class firstDemoTest {
+    @Test
+    public void subtractTest() {
+        assertEquals(2, firstDemo.subtract(2, 1));
+    }
+}
